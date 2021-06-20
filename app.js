ff9b7d32ed461b7a488b85b8c672dda7afb1fed6
halamahmoud <85875349+hala-89@users.noreply.github.com>
2021-06-20 12:49:10 +0000

    Funcation

--- app.js
+++ app.js
@@ -4,12 +4,14 @@ var name = prompt(" Hello , what is  your name?")
 
 
 alert ("welcom to fantasy moblie website " +name )
-
-var x = prompt("what is your fav type of moblie from these (Samsung, iphon ,HUAWEI)?")
+  
+  function favmoblie(){
+  
+  var x = prompt("what is your fav type of moblie from these (Samsung, iphon ,HUAWEI)?")
 
  
 while (x != "samsung" && x != "iphon" && x != "HUAWEI" )
-{
+{   
  x = prompt(" Sorry ! you entered wrong input please try again (samsung, iphon ,HUAWEI)  ")
 } 
 if (x == "samsung"){
@@ -25,8 +27,9 @@ for (var m= 1; m<= numofpic; m++)
 
 for (var m= 1; m<= numofpic; m++){
   document.write("<p>"+"great thing you love  iphon becuase is smart phone to use" +"</p>" +'<div>'+'<h3>'+'iphon'+'</h3>'+"<img src='https://media.voltron.alhurra.com/Drupal/01live-106/2020-09/960x0%20%281%29.jpg'</div>")
-  }	
+}
   }
+  
 
    else if( x =="HUAWEI"){
      if(x ==  "HUAWEI"){var numofpic = prompt (" How many HUAWEI do you want to see at our site ?")}
@@ -36,8 +39,35 @@ for (var m= 1; m<= numofpic; m++)
   document.write("<p>"+"great thing you love HUAWEI becuase is easy phone to use" +"</p>" +'<div>'+'<h3>'+'iphon'+'</h3>'+"<img src='https://static.huaweieshop.jo/uploads/thumbs/73/7307dd71f1c6ea0416f4375ac469eb65.jpg'</div>")
    }
    }
-
+  
 else{ alert("sorry we don't have this type")}
+  
+ 
+
+var starsnum = prompt('how many stras do you rate websit ?')
+      
+
+function rating(num){
+
+        for (var i =0 ; i< num; i++){
+
+      
+    document.write("<img src= https://static4.depositphotos.com/1026550/376/i/950/depositphotos_3763236-stock-photo-gold-star.jpg")
+        
+
+
+  
+      
+}
+}
+
+
+rating(starsnum);
+
+  }
+  favmoblie();
+
+
 
 alert("Thank you for visiting my website")
-
+  
